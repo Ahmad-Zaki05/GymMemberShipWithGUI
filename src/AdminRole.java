a46d1ef84aea5aa0dabd83c4a10ead98173f9756
AE107 <deadrenderdigital@gmail.com>
2024-10-24 15:27:07 +0300

    adding classes

--- src/AdminRole.java
+++ src/AdminRole.java
@@ -1,7 +1,21 @@
 public class AdminRole {
     private TrainerDatabase database;
 
-    public void addTrainer(Trainer T ){
+    public void addTrainer(String trainerId, String name, String email, String specialty, String phoneNumber) {
+        Trainer trainer = new Trainer(trainerId, name, email, specialty, phoneNumber);
+
 
     }
+
+    public TrainerDatabase getListOfTrainers() {
+        return database;
+    }
+
+    public void removeTrainer(String trainerId) {
+
+        database.deleteRecord(trainerId);
+
+    }
+
+
 }
--- src/TrainerDatabase.java
+++ src/TrainerDatabase.java
@@ -3,6 +3,45 @@ import java.util.ArrayList;
 public class TrainerDatabase {
 
     private ArrayList<Trainer> records;
-    private String fileName;
+    private final String fileName;
+
+    public TrainerDatabase() {
+        this.fileName = "Trainers.txt";
+        records = new ArrayList<Trainer>();
+    }
+
+    public Trainer createRecordFrom (String line) {
+        String[] parts = line.split(",");
+        return new Trainer(parts[0], parts[1], parts[2], parts[3], parts[4]);
+    }
+
+    public ArrayList<Trainer> returnAllRecords() {
+        return records;
+    }
+
+    public Trainer getRecord(String key) {
+        for (Trainer record : records) {
+            if (record.getSearchKey().equals(key)) {
+                return record;
+            }
+        }
+        return null;
+    }
+
+    public void insertRecord(Trainer record) {
+        records.add(record);
+    }
+    public void deleteRecord(String key) {
+        Trainer record = getRecord(key);
+        if (record != null) {
+            records.remove(record);
+        }
+    }
+    public void readFromFile() {
+        // read from file
+    }
+    public void saveToFile() {
+        // write to file
+    }
 
 }
