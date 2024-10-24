653d1e136bba08c2b34030e533a52cedfb964b6d
AE107 <deadrenderdigital@gmail.com>
2024-10-24 23:47:09 +0300

    fixed bug with txt

--- src/Lab4.java
+++ src/Lab4.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Lab4 {
     public static void main(String[] args) {
         while(true){
             System.out.println("please Select operation mode");
             System.out.println("1. Admin");
             System.out.println("2. Trainer");
+            System.out.println("3.Quit");
         }
     }
 }
