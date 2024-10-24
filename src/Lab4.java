10f812f7b961f1ad40a60b3ee2632257fdcbadf6
AE107 <deadrenderdigital@gmail.com>
2024-10-24 13:35:59 +0300

    adding classes

--- src/Lab4.java
+++ src/Lab4.java
@@ -1,5 +1,5 @@
 public class Lab4 {
     public static void main(String[] args) {
-        //hello, still haven't done anything here
+        // create options menu later
     }
 }
---
+++ src/MemberDatabase.java
@@ -0,0 +1,2 @@
+public class MemberDatabase {
+}
