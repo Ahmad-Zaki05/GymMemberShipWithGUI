14cb81f2dc50b45391180e193afb681ac920ad85
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-10-31 23:47:34 +0300

    Update FileNames.java
    minor change to class file name to correct bug

--- src/constants/FileNames.java
+++ src/constants/FileNames.java
@@ -3,6 +3,6 @@ package constants;
 public interface FileNames {
     String TRAINER_FILENAME = "Trainers.txt";
     String MEMBER_FILENAME = "Members.txt";
-    String CLASS_FILENAME = "Class.txt";
+    String CLASS_FILENAME = "Classes.txt";
     String REGISTRATION_FILENAME = "Registration.txt";
 }
